1b25a915a2a31da3046968cbec9c17dcc2bbfaa5
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-03-26 11:19:06 +0100

    add creation of montage

--- project_hnp/krios/io.py
+++ project_hnp/krios/io.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 import pandas as pd
+from mne.channels import make_dig_montage
 from pycpd import RigidRegistration
 
 from ..utils._checks import ensure_path
@@ -13,12 +14,16 @@ from ._transform import krios_to_head_coordinate, reorder_electrodes
 if TYPE_CHECKING:
     from pathlib import Path
 
+    from mne.channels import DigMontage
     from numpy.typing import NDArray
 
 
 _LPA_LABEL: str = "LEFT_PP_MARKER"
 _RPA_LABEL: str = "RIGHT_PP_MARKER"
 _NZ_LABEL: str = "NASION_MARKER"
+_TEMPLATE_FNAME: Path = (
+    files("project_hnp.krios") / "assets" / "EGI 257.Geneva Average 13.10-10__.xyz"
+)
 
 
 def read_krios(fname: Path | str) -> tuple[NDArray[np.float64], NDArray[np.float64]]:
@@ -51,29 +56,48 @@ def read_krios(fname: Path | str) -> tuple[NDArray[np.float64], NDArray[np.float
     del df
     elc, fid, _ = krios_to_head_coordinate(elc, rpa=rpa, lpa=lpa, nz=nz)
     # read template file
-    template_fname = (
-        files("project_hnp.krios") / "assets" / "EGI 257.Geneva Average 13.10-10__.xyz"
-    )
-    df_template = pd.read_csv(template_fname, sep=" ", header=0, skipinitialspace=True)
+    df_template = pd.read_csv(_TEMPLATE_FNAME, sep=" ", header=0, skipinitialspace=True)
     elc_template = df_template.loc[:, ["x", "y", "z"]].to_numpy()
     # co-register with template
-    # RigidRegistration(...).register() returns 4 elements:
+    # RigidRegistration(...).register() returns 2 elements:
     # - TY : array of shape (n_points, 3), the registered and transformed source points
-    # - s_reg : float, the scale of the registration
-    # - R_reg : array of shape (3, 3), the rotation matrix of the registration
-    # - t_reg : array of shape (1, 3), the translation vector of the registration
+    # - registration parameters : tuple of 3 elements
+    #   - s_reg : float, the scale of the registration
+    #   - R_reg : array of shape (3, 3), the rotation matrix of the registration
+    #   - t_reg : array of shape (1, 3), the translation vector of the registration
     elc_TY, _ = RigidRegistration(X=elc_template, Y=elc).register()  # TODO: check
     # reorder electrodes according to template
     elc_reordered = reorder_electrodes(elc_TY, elc_template)
     return elc_reordered, fid
 
 
-def read_krios_montage(fname: Path | str):
+def read_krios_montage(fname: Path | str) -> DigMontage:
     """Read electrode locations from a Krios file in a DigMontage.
 
     Parameters
     ----------
     fname : Path | str
         Path to the Krios file.
+
+    Returns
+    -------
+    montage : DigMontage
+        MNE channel montage for the EEG electrodes in the head coordinate frame.
     """
     elc_reordered, fid = read_krios(fname)
+    df_template = pd.read_csv(_TEMPLATE_FNAME, sep=" ", header=0, skipinitialspace=True)
+    ch_names = df_template["labels"].values
+    del df_template
+    if elc_reordered.shape[0] != ch_names.size:
+        raise ValueError(
+            f"Number of electrodes ({elc_reordered.shape[0]}) does not match the "
+            f"number of channel names ({ch_names.size}) in the template."
+        )
+    ch_pos = dict(zip(ch_names, elc_reordered, strict=True))
+    return make_dig_montage(
+        ch_pos=ch_pos,
+        nasion=fid[2, :],
+        lpa=fid[1, :],
+        rpa=fid[0, :],
+        coord_frame="head",
+    )
