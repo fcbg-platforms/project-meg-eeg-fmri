7cba7672c9b0dfddc9c6c78e36296086e807ec1f
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-03-26 10:45:51 +0100

    add krios I/O

---
+++ project_hnp/krios/_transform.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+from warnings import warn
+
+import numpy as np
+from scipy.optimize import linear_sum_assignment
+from scipy.spatial import distance_matrix
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+
+
+def krios_to_head_coordinate(
+    elc: NDArray[np.float64],
+    rpa: NDArray[np.float64],
+    lpa: NDArray[np.float64],
+    nz: NDArray[np.float64],
+) -> tuple[NDArray[np.float64], NDArray[np.float64]]:
+    """Transform Krios coordinates to head coordinates.
+
+    Parameters
+    ----------
+    elc : array of shape (n_electrodes, 3)
+        (x, y, z) coordinates of the EEG electrodes.
+    rpa : array of shape (3,)
+        (x, y, z) coordinates of the right pre-auricular point.
+    lpa : array of shape (3,)
+        (x, y, z) coordinates of the left pre-auricular point.
+    nz : array of shape (3,)
+        (x, y, z) coordinates of the nasion.
+
+    Returns
+    -------
+    elc : array of shape (n_electrodes, 3)
+        (x, y, z) coordinates of the EEG electrodes in the head coordinate system.
+    fid : array of shape (3, 3)
+        (x, y, z) coordinates of the fiducials in the head coordinate system, ordered as
+        RPA, LPA, NZ.
+    T : array of shape (3, 3)
+        Transformation matrix from the Krios to the head coordinate system.
+    """
+    for var in (elc, rpa, lpa, nz):  # sanity-checks
+        assert var.dtype == np.float64
+        assert var.shape[-1] == 3
+    assert elc.ndim == 2
+    assert rpa.ndim == 1
+    assert lpa.ndim == 1
+    assert nz.ndim == 1
+    # define coordinate system
+    x = rpa - lpa  # left to right axis
+    xy = nz - lpa  # vector on the xy plan
+    z = np.cross(x, xy)  # bottom to top axis = normal to xy plan
+    y = np.cross(z, x)  # back to front axis = normal to zx plan
+    x = x / np.linalg.norm(x, 2)
+    y = y / np.linalg.norm(y, 2)
+    z = z / np.linalg.norm(z, 2)
+    # create transformation matrix from Krios to head coordinate system
+    T = np.zeros(shape=(3, 3), dtype=np.float64)
+    T[0, :] = x
+    T[1, :] = y
+    T[2, :] = z
+    # apply transformation to electrodes and fiducials
+    elc = np.matmul(T, elc.T).T
+    fid = np.matmul(T, np.array([rpa, lpa, nz]).T).T
+    # change origin
+    origin_shift = [fid[2, 0], fid[0, 1], fid[2, 2]]
+    elc = elc - np.tile(origin_shift, (elc.shape[0], 1))
+    return elc, fid, T
+
+
+def reorder_electrodes(
+    elc: NDArray[np.float64], template: NDArray[np.float64]
+) -> NDArray[np.float64]:
+    """Re-order electrodes according to the template.
+
+    Parameters
+    ----------
+    elc : array of shape (n_electrodes, 3)
+        (x, y, z) coordinates of the EEG electrodes.
+    template : array of shape (n_electrodes, 3)
+        (x, y, z) coordinates of the EEG electrodes in the template.
+
+    Returns
+    -------
+    elc_reordered : array of shape (n_electrodes, 3)
+        Reordered electrode locations matching the template order.
+    """
+    elc_reordered = np.zeros(template.shape, dtype=np.float64)
+    cost_matrix = distance_matrix(template, elc, p=2)
+    r_idx, c_idx = linear_sum_assignment(cost_matrix)
+    elc_reordered[r_idx, :] = elc[c_idx, :]
+    # check if there is less scanned electrodes than template electrode, in which case
+    # add electrode location from the template directly
+    if r_idx.size < template.shape[0]:
+        z = np.in1d(np.arange(template.shape[0]), r_idx, assume_unique=False)
+        idx = np.nonzero(~z)[0]
+        elc_reordered[idx, :] = template[idx, :]
+        warn(
+            "Some electrodes are missing in the scanned data and have been filled from "
+            "the template.",
+            RuntimeWarning,
+            stacklevel=3,
+        )
+    return elc_reordered
---
+++ project_hnp/krios/io.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+from importlib.resources import files
+from typing import TYPE_CHECKING
+
+import numpy as np
+import pandas as pd
+from pycpd import RigidRegistration
+
+from ..utils._checks import ensure_path
+from ._transform import krios_to_head_coordinate, reorder_electrodes
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from numpy.typing import NDArray
+
+
+_LPA_LABEL: str = "LEFT_PP_MARKER"
+_RPA_LABEL: str = "RIGHT_PP_MARKER"
+_NZ_LABEL: str = "NASION_MARKER"
+
+
+def read_krios(fname: Path | str) -> tuple[NDArray[np.float64], NDArray[np.float64]]:
+    """Read electrode locations from a Krios file.
+
+    Parameters
+    ----------
+    fname : Path | str
+        Path to the Krios file.
+
+    Returns
+    -------
+    elc : array of shape (n_electrodes, 3)
+        (x, y, z) coordinates of the EEG electrodes in the head coordinate system.
+    fid : array of shape (3, 3)
+        (x, y, z) coordinates of the fiducials in the head coordinate system, ordered as
+        RPA, LPA, NZ.
+    """
+    fname = ensure_path(fname, must_exist=True)
+    df = pd.read_csv(fname, sep=",", header=0)
+    eeg_idx = (df["Special Sensor"] == "EEG").to_numpy()
+    lpa_idx = (df["Special Sensor"] == _LPA_LABEL).to_numpy()
+    rpa_idx = (df["Special Sensor"] == _RPA_LABEL).to_numpy()
+    nz_idx = (df["Special Sensor"] == _NZ_LABEL).to_numpy()
+    # extract (x, y, z) coordinates as array of shape (n_scanned, 3)
+    elc = df.loc[eeg_idx, ["x(cm)", "y(cm)", "z(cm)"]].to_numpy()
+    rpa = np.squeeze(df.loc[rpa_idx, ["x(cm)", "y(cm)", "z(cm)"]].to_numpy())
+    lpa = np.squeeze(df.loc[lpa_idx, ["x(cm)", "y(cm)", "z(cm)"]].to_numpy())
+    nz = np.squeeze(df.loc[nz_idx, ["x(cm)", "y(cm)", "z(cm)"]].to_numpy())
+    del df
+    elc, fid, _ = krios_to_head_coordinate(elc, rpa=rpa, lpa=lpa, nz=nz)
+    # read template file
+    template_fname = (
+        files("project_hnp.krios") / "assets" / "EGI 257.Geneva Average 13.10-10__.xyz"
+    )
+    df_template = pd.read_csv(template_fname, sep=" ", header=0, skipinitialspace=True)
+    elc_template = df_template.loc[:, ["x", "y", "z"]].to_numpy()
+    # co-register with template
+    # RigidRegistration(...).register() returns 4 elements:
+    # - TY : array of shape (n_points, 3), the registered and transformed source points
+    # - s_reg : float, the scale of the registration
+    # - R_reg : array of shape (3, 3), the rotation matrix of the registration
+    # - t_reg : array of shape (1, 3), the translation vector of the registration
+    elc_TY, _, _, _ = RigidRegistration(X=elc_template, Y=elc).register()  # TODO: check
+    # reorder electrodes according to template
+    elc_reordered, _ = reorder_electrodes(elc_TY, elc_template)
+    return elc_reordered, fid
