5d23a23c2827017b3227605e1cdb73fa1c9bb12d
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-03-26 16:15:36 +0100

    add tests

--- project_hnp/krios/tests/test_utils.py
+++ project_hnp/krios/tests/test_utils.py
@@ -14,7 +14,10 @@ def test_remove_duplicates():
         [[1, 1, 1], [1.1, 1.1, 1.1], [2, 2, 2], [3, 3, 3], [4, 4, 4], [5, 5, 5]]
     )
     points_filtered = remove_duplicates(points2, threshold=0.5)
-    assert_allclose(points, desired=points_filtered)
+    assert_allclose(
+        np.array([[1.05, 1.05, 1.05], [2, 2, 2], [3, 3, 3], [4, 4, 4], [5, 5, 5]]),
+        desired=points_filtered,
+    )
     points_filtered = remove_duplicates(points2, threshold=0.001)
     assert_allclose(points2, desired=points_filtered)
 
@@ -46,7 +49,18 @@ def test_remove_duplicates():
         ]
     )
     points_filtered = remove_duplicates(points4, threshold=0.5)
-    assert_allclose(points, desired=points_filtered)
+    assert_allclose(
+        np.array(
+            [
+                [1.05, 1.05, 1.05],
+                [4.05, 4.05, 4.05],
+                [2, 2, 2],
+                [3, 3, 3],
+                [5, 5, 5],
+            ]
+        ),
+        desired=points_filtered,
+    )
     points_filtered = remove_duplicates(points4, threshold=0.001)
     assert_allclose(points4, desired=points_filtered)
 
@@ -54,18 +68,46 @@ def test_remove_duplicates():
         [
             [1, 1, 1],
             [1.1, 1.1, 1.1],
-            [1.11, 1.1, 1.1],
+            [1.2, 1.2, 1.2],
+            [0.9, 0.9, 0.9],
             [2, 2, 2],
-            [4.1, 4.11, 4.1],
+            [4.1, 4.1, 4.1],
             [3, 3, 3],
-            [1.1, 1.1, 1.11],
+            [0.8, 0.8, 0.8],
             [4, 4, 4],
             [4.1, 4.1, 4.1],
             [5, 5, 5],
-            [4.1, 4.1, 4.11],
+            [4, 4, 4],
         ]
     )
     points_filtered = remove_duplicates(points5, threshold=0.5)
-    assert_allclose(points, desired=points_filtered)
+    assert_allclose(
+        np.array(
+            [
+                [1, 1, 1],
+                [4.05, 4.05, 4.05],
+                [2, 2, 2],
+                [3, 3, 3],
+                [5, 5, 5],
+            ]
+        ),
+        desired=points_filtered,
+    )
     points_filtered = remove_duplicates(points5, threshold=0.001)
-    assert_allclose(points5, desired=points_filtered)
+    assert_allclose(
+        np.array(
+            [
+                [4.1, 4.1, 4.1],
+                [4, 4, 4],
+                [1, 1, 1],
+                [1.1, 1.1, 1.1],
+                [1.2, 1.2, 1.2],
+                [0.9, 0.9, 0.9],
+                [2, 2, 2],
+                [3, 3, 3],
+                [0.8, 0.8, 0.8],
+                [5, 5, 5],
+            ]
+        ),
+        desired=points_filtered,
+    )
