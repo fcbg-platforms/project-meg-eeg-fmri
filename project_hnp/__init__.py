a2c1128b83158fb311a260c2278fb5e1b3efeb98
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-03-27 13:49:14 +0100

    fix imports

--- project_hnp/__init__.py
+++ project_hnp/__init__.py
@@ -1,2 +1,4 @@
 from . import bids, krios, utils
+from ._version import __version__
+from .utils.config import sys_info
 from .utils.logs import add_file_handler, logger, set_log_level
--- project_hnp/bids/__init__.py
+++ project_hnp/bids/__init__.py
@@ -1,4 +1,5 @@
 from . import eeg, io, meg, mri
 from .eeg import write_eeg_datasets
+from .io import bidsification
 from .meg import write_meg_datasets
 from .mri import write_mri_datasets
