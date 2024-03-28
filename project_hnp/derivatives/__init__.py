7f49c4f4cb2aa677572f46fcde2a593b011db01d
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-03-28 14:24:51 +0100

    fix imports

--- project_hnp/derivatives/__init__.py
+++ project_hnp/derivatives/__init__.py
@@ -1,4 +1,4 @@
-from . import chpi, freesurfer, montage
+from . import cartool, chpi, freesurfer
+from .cartool import export_krios_digitization
 from .chpi import compute_heas_pos
 from .freesurfer import mri_convert, recon_all
-from .montage import export_krios_digitization
